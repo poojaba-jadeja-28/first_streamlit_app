bcdd0e9c295d6ba35a0ecc6c18b67a8d4a6b113d
poojaba-jadeja-28 <72756522+poojaba-jadeja-28@users.noreply.github.com>
2023-09-08 13:43:20 +0530

    Update ui.py

--- ui.py
+++ ui.py
@@ -1,3 +1,48 @@
 import streamlit
 
+from streamlit_calendar import calendar
+
 streamlit.title('PRICING UI TRIAL')
+
+calendar_options = {
+    "headerToolbar": {
+        "left": "today prev,next",
+        "center": "title",
+        "right": "resourceTimelineDay,resourceTimelineWeek,resourceTimelineMonth",
+    },
+    "slotMinTime": "06:00:00",
+    "slotMaxTime": "18:00:00",
+    "initialView": "resourceTimelineDay",
+    "resourceGroupField": "building",
+    "resources": [
+        {"id": "a", "building": "Building A", "title": "Building A"},
+        {"id": "b", "building": "Building A", "title": "Building B"},
+        {"id": "c", "building": "Building B", "title": "Building C"},
+        {"id": "d", "building": "Building B", "title": "Building D"},
+        {"id": "e", "building": "Building C", "title": "Building E"},
+        {"id": "f", "building": "Building C", "title": "Building F"},
+    ],
+}
+calendar_events = [
+    {
+        "title": "Event 1",
+        "start": "2023-07-31T08:30:00",
+        "end": "2023-07-31T10:30:00",
+        "resourceId": "a",
+    },
+    {
+        "title": "Event 2",
+        "start": "2023-07-31T07:30:00",
+        "end": "2023-07-31T10:30:00",
+        "resourceId": "b",
+    },
+    {
+        "title": "Event 3",
+        "start": "2023-07-31T10:40:00",
+        "end": "2023-07-31T12:30:00",
+        "resourceId": "a",
+    },
+]
+
+calendar = calendar(events=calendar_events, options=calendar_options)
+st.write(calendar)
