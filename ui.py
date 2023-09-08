021102eb1350675f83ca137840676a97c9caa1f0
poojaba-jadeja-28 <72756522+poojaba-jadeja-28@users.noreply.github.com>
2023-09-08 14:30:36 +0530

    Update ui.py

--- ui.py
+++ ui.py
@@ -1,7 +1,53 @@
 import streamlit as st
+pip install streamlit-calendar
 
-# Create a dropdown menu with a list of countries
-selected_country = st.selectbox('Select a country:', ['USA', 'Canada', 'UK', 'Australia'])
+from streamlit_calendar import calendar
 
-# Display the selected country
-st.write('You selected:', selected_country)
+calendar_options = {
+    "headerToolbar": {
+        "left": "today prev,next",
+        "center": "title",
+        "right": "resourceTimelineDay,resourceTimelineWeek,resourceTimelineMonth",
+    },
+    "slotMinTime": "06:00:00",
+    "slotMaxTime": "18:00:00",
+    "initialView": "resourceTimelineDay",
+    "resourceGroupField": "building",
+    "resources": [
+        {"id": "a", "building": "Building A", "title": "Building A"},
+        {"id": "b", "building": "Building A", "title": "Building B"},
+        {"id": "c", "building": "Building B", "title": "Building C"},
+        {"id": "d", "building": "Building B", "title": "Building D"},
+        {"id": "e", "building": "Building C", "title": "Building E"},
+        {"id": "f", "building": "Building C", "title": "Building F"},
+    ],
+}
+calendar_events = [
+    {
+        "title": "Event 1",
+        "start": "2023-07-31T08:30:00",
+        "end": "2023-07-31T10:30:00",
+        "resourceId": "a",
+    },
+    {
+        "title": "Event 2",
+        "start": "2023-07-31T07:30:00",
+        "end": "2023-07-31T10:30:00",
+        "resourceId": "b",
+    },
+    {
+        "title": "Event 3",
+        "start": "2023-07-31T10:40:00",
+        "end": "2023-07-31T12:30:00",
+        "resourceId": "a",
+    },
+]
+
+calendar = calendar(events=calendar_events, options=calendar_options)
+st.write(calendar)
+
+# # Create a dropdown menu with a list of countries
+# selected_country = st.selectbox('Select a country:', ['USA', 'Canada', 'UK', 'Australia'])
+
+# # Display the selected country
+# st.write('You selected:', selected_country)
